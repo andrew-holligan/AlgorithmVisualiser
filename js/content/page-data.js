a53d6f6a28f83a2c3701b96a9546589b56d76f70
Andrew Holligan <2771095h@student.gla.ac.uk>
2024-02-16 16:15:21 +0000

    feat: init quick sort

--- js/content/page-data.js
+++ js/content/page-data.js
@@ -1,9 +1,11 @@
 import { bubbleSortData } from "./bubble-sort-data.js";
 import { insertionSortData } from "./insertion-sort-data.js";
 import { mergeSortData } from "./merge-sort-data.js";
+import { quickSortData } from "./quick-sort-data.js";
 
 export const pageData = new Map();
 
 pageData.set("bubble-sort.html", bubbleSortData);
 pageData.set("insertion-sort.html", insertionSortData);
 pageData.set("merge-sort.html", mergeSortData);
+pageData.set("quick-sort.html", quickSortData);
---
+++ js/content/quick-sort-data.js
@@ -0,0 +1,8 @@
+export const quickSortData = new Map();
+
+quickSortData.set("javascript", ``);
+quickSortData.set("java", ``);
+quickSortData.set("python", ``);
+quickSortData.set("cpp", `STILL TO IMPLEMENT`);
+quickSortData.set("csharp", `STILL TO IMPLEMENT`);
+quickSortData.set("c", `STILL TO IMPLEMENT`);
