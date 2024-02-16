a2061c5ab0b52191c4972687dc5bceb3b98cce0c
Andrew Holligan <2771095h@student.gla.ac.uk>
2024-02-16 15:53:00 +0000

    feat: init merge sort

---
+++ js/content/merge-sort-data.js
@@ -0,0 +1,8 @@
+export const mergeSortData = new Map();
+
+mergeSortData.set("javascript", ``);
+mergeSortData.set("java", ``);
+mergeSortData.set("python", ``);
+mergeSortData.set("cpp", `STILL TO IMPLEMENT`);
+mergeSortData.set("csharp", `STILL TO IMPLEMENT`);
+mergeSortData.set("c", `STILL TO IMPLEMENT`);
--- js/content/page-data.js
+++ js/content/page-data.js
@@ -1,7 +1,9 @@
 import { bubbleSortData } from "./bubble-sort-data.js";
 import { insertionSortData } from "./insertion-sort-data.js";
+import { mergeSortData } from "./merge-sort-data.js";
 
 export const pageData = new Map();
 
 pageData.set("bubble-sort.html", bubbleSortData);
 pageData.set("insertion-sort.html", insertionSortData);
+pageData.set("merge-sort.html", mergeSortData);
