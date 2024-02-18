734b3ad197d251392a977d2a628c0c480ac66d59
Andrew Holligan <2771095h@student.gla.ac.uk>
2024-02-18 11:20:00 +0000

    testing: this commit is only for testing purposes for JSON not working mobile

--- js/page/pages/bubble-sort-page.js
+++ js/page/pages/bubble-sort-page.js
@@ -3,9 +3,12 @@ import { PageContent } from "../../content/parser/page-content.js";
 
 import { BubbleSort } from "../../algorithm/bubble-sort.js";
 
+console.log("importing json");
 const { default: json } = await import("../../content/json/bubble-sort.json", {
   assert: { type: "json" },
 });
+console.log("finished importing json");
+console.log(json);
 
 const pageContent = new PageContent(json);
 const bubbleSortPage = new AlgorithmPage(BubbleSort, pageContent);
